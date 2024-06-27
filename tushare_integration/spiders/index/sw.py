6f28f6a4648cdf1ba41d4dfbd38883f8b7ae9074
zhangbingchen <1731259685@qq.com>
2024-06-27 23:11:54 +0800

    add sw data

--- tushare_integration/spiders/index/sw.py
+++ tushare_integration/spiders/index/sw.py
@@ -1,4 +1,6 @@
-from tushare_integration.spiders.tushare import TushareSpider
+from urllib import request
+
+from tushare_integration.spiders.tushare import DailySpider, TushareSpider
 
 
 class IndexClassifySpider(TushareSpider):
@@ -20,3 +22,33 @@ class IndexMemberSpider(TushareSpider):
                     'index_code': index_code,
                 }
             )
+
+
+class IndexMemberAllSpider(TushareSpider):
+    name = "index/sw/index_member_all"
+    custom_settings = {"TABLE_NAME": "index_member_all"}
+
+    def start_requests(self):
+        # 通过LIMIT+OFFSET的方式取数据
+        request = self.get_scrapy_request(params={'offset': 0, 'limit': 3000})
+        request.meta["offset"] = 0
+        request.meta["limit"] = 3000
+        yield request
+
+    def parse(self, response, **kwargs):
+        parsed_data = self.parse_response(response, **kwargs)
+        if parsed_data["data"].empty:
+            return
+        yield parsed_data
+
+        request = self.get_scrapy_request(
+            params={'offset': response.meta["offset"] + response.meta["limit"], 'limit': response.meta["limit"]}
+        )
+        request.meta["offset"] = response.meta["offset"] + response.meta["limit"]
+        request.meta["limit"] = response.meta["limit"]
+        yield request
+
+
+class SWDailySpider(DailySpider):
+    name = "index/sw/sw_daily"
+    custom_settings = {"TABLE_NAME": "sw_daily"}
