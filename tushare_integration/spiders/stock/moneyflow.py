addc55bf35ea52d50eb16c272db3023aa5fa690a
zhangbingchen004 <zhangbingchen004@ke.com>
2024-10-17 15:23:21 +0800

    Refactor moneyflow spider custom settings

--- tushare_integration/spiders/stock/moneyflow.py
+++ tushare_integration/spiders/stock/moneyflow.py
@@ -13,34 +13,24 @@ class MoneyFlowHSGTSpider(DailySpider):
 
 class MoneyFlowDCSpider(DailySpider):
     name = "stock/moneyflow/moneyflow_dc"
-    custom_settings = {
-        "TABLE_NAME": "moneyflow_dc",
-    }
+    custom_settings = {"TABLE_NAME": "moneyflow_dc", "MIN_CAL_DATE": "2023-09-11"}
 
 
 class MoneyFlowIndDCSpider(DailySpider):
     name = "stock/moneyflow/moneyflow_ind_dc"
-    custom_settings = {
-        "TABLE_NAME": "moneyflow_ind_dc",
-    }
+    custom_settings = {"TABLE_NAME": "moneyflow_ind_dc", "MIN_CAL_DATE": "2023-09-12"}
 
 
 class MoneyFlowIndTHSSpider(DailySpider):
     name = "stock/moneyflow/moneyflow_ind_ths"
-    custom_settings = {
-        "TABLE_NAME": "moneyflow_ind_ths",
-    }
+    custom_settings = {"TABLE_NAME": "moneyflow_ind_ths", "MIN_CAL_DATE": "2024-09-10"}
 
 
 class MoneyFlowMktDCSpider(DailySpider):
     name = "stock/moneyflow/moneyflow_mkt_dc"
-    custom_settings = {
-        "TABLE_NAME": "moneyflow_mkt_dc",
-    }
+    custom_settings = {"TABLE_NAME": "moneyflow_mkt_dc", "MIN_CAL_DATE": "2023-04-17"}
 
 
 class MoneyFlowTHSSpider(DailySpider):
     name = "stock/moneyflow/moneyflow_ths"
-    custom_settings = {
-        "TABLE_NAME": "moneyflow_ths",
-    }
+    custom_settings = {"TABLE_NAME": "moneyflow_ths", "MIN_CAL_DATE": "2019-07-26"}
